080a04fa6ef579877d43ae88419d8921b21b9eac
Anonymous <StratosBlue@outlook.com>
2020-01-10 15:35:21 +0800

    调整HttpReports.Dashboard项目以支持core2.1、core3.0、core3.1; (core2.2官方已停止支持)

--- src/HttpReports.Dashboard/Program.cs
+++ src/HttpReports.Dashboard/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 
 namespace HttpReports.Dashboard
 {
@@ -14,11 +8,24 @@ namespace HttpReports.Dashboard
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+#if NETCOREAPP2_1
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+#elif NETCOREAPP3_0 || NETCOREAPP3_1
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+
+#endif
     }
 }
