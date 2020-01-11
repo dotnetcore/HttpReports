8a1419e93ea0bd84db0ac4e8debc65a72b74651c
Spring Lee <1028789852@qq.com>
2020-01-11 19:07:51 +0800

    commit

--- example/WebApiServer/Startup.cs
+++ example/WebApiServer/Startup.cs
@@ -31,7 +31,7 @@ namespace WebApiServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-             app.UseHttpReports();
+            app.UseHttpReports();
 
             if (env.IsDevelopment())
             {
