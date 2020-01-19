60ce31aad219940945dc766a9ebda7f0dcaddc2c
StratosBlue <StratosBlue@outlook.com>
2020-01-19 10:54:30 +0800

    整理项目, 移除不再使用的类等;

--- src/HttpReports.Dashboard/Models/GetIndexDataRequest.cs
+++ src/HttpReports.Dashboard/Models/GetIndexDataRequest.cs
@@ -21,38 +21,4 @@
         /// </summary>
         public bool IsDesc { get; set; }
     }
-
-    public class GetIndexDataResponse
-    {
-        /// <summary>
-        /// 平均处理时间
-        /// </summary>
-        public string ART { get; set; }
-
-        /// <summary>
-        /// 总请求次数
-        /// </summary>
-        public string Total { get; set; }
-
-        /// <summary>
-        /// 404
-        /// </summary>
-        public string Code404 { get; set; }
-
-        /// <summary>
-        /// 500
-        /// </summary>
-
-        public string Code500 { get; set; }
-
-        /// <summary>
-        /// 错误占比
-        /// </summary>
-        public string ErrorPercent { get; set; }
-
-        /// <summary>
-        /// 接口数量 - 已调用
-        /// </summary>
-        public int APICount { get; set; }
-    }
 }
