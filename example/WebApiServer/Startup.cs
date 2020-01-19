0786b5a4f62aa9e55a5e868b0b9b53a55038cc49
StratosBlue <StratosBlue@outlook.com>
2020-01-19 17:38:01 +0800

    修正MySqlStorage注册为Transient, 导致延时数据写入异常的问题;

--- example/WebApiServer/Startup.cs
+++ example/WebApiServer/Startup.cs
@@ -24,9 +24,9 @@ namespace WebApiServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers(); 
-             
-            services.AddHttpReports().UseWebApiReports().UseOracleStorage();
+            services.AddControllers();
+
+            services.AddHttpReports().UseWebApiReports().UseMySqlStorage();
             //services.AddHttpReports(Configuration.GetSection("HttpReports")).UseWebApiReports().UseMySqlStorage();
         }
 
--- src/HttpReports.Storage.MySQL/ServiceBuildExtensions.cs
+++ src/HttpReports.Storage.MySQL/ServiceBuildExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
+
 using HttpReports;
 using HttpReports.Storage.MySql;
 
-[assembly:InternalsVisibleTo("HttpReports.Test")]
+[assembly: InternalsVisibleTo("HttpReports.Test")]
+
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceBuildExtensions
@@ -11,7 +13,7 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             builder.Services.AddOptions();
             builder.Services.Configure<MySqlStorageOptions>(builder.Configuration.GetSection("Storage"));
-            builder.Services.AddTransient<IHttpReportsStorage, MySqlStorage>();
+            builder.Services.AddSingleton<IHttpReportsStorage, MySqlStorage>();
             builder.Services.AddSingleton<MySqlConnectionFactory>();
 
             return builder;
