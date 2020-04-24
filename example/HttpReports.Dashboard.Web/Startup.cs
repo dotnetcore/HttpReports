82c63adce2ceb03797c6fabaa9e55568eb3a2247
Li Shuai <lishuai@diyibox.com>
2020-04-24 13:11:24 +0800

    Dashboard 调整

--- example/HttpReports.Dashboard.Web/Startup.cs
+++ example/HttpReports.Dashboard.Web/Startup.cs
@@ -24,7 +24,7 @@ namespace HttpReports.Dashboard.Web
         {
             services.AddHttpReports().UseMySqlStorage().UseGrpc(); 
 
-            services.AddHttpReportsDashborad().UseMySqlStorage();
+            services.AddHttpReportsDashboard().UseMySqlStorage();
            
             services.AddControllersWithViews();
         }
