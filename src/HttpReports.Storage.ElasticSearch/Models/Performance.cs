23a01f8b97b2ca75c13b141768dbe0e3ac653a06
Li Shuai <lishuai@diyibox.com>
2020-06-11 11:52:17 +0800

    1

--- src/HttpReports.Storage.ElasticSearch/Models/Performance.cs
+++ src/HttpReports.Storage.ElasticSearch/Models/Performance.cs
@@ -9,27 +9,47 @@ namespace HttpReports.Storage.ElasticSearch.Models
     [ElasticsearchType(RelationName = "performance")]
     public class Performance : IPerformance
     {
+        [Nest.Keyword]
         public string Id { get; set; }
+
+        [Nest.Keyword]
         public string Service { get; set; }
 
+        [Nest.Keyword] 
         public string Instance { get; set; }
 
+        [Nest.Number] 
         public int GCGen0 { get; set; }
 
+
+        [Nest.Number]
         public int GCGen1 { get; set; }
 
+
+        [Nest.Number]
         public int GCGen2 { get; set; }
 
+        [Nest.Number]
         public double HeapMemory { get; set; }
 
+
+        [Nest.Number]
         public double ProcessCPU { get; set; }
 
+
+        [Nest.Number]
         public double ProcessMemory { get; set; }
 
+
+        [Nest.Number]
         public int ThreadCount { get; set; }
 
+
+        [Nest.Number]
         public int PendingThreadCount { get; set; }
 
+
+        [Nest.Date]
         public DateTime CreateTime { get; set; }
 
     }
