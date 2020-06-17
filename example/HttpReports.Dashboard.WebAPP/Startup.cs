a4d073038ab288cc7b334be09fa3a412ee28d0e5
Spring Lee <1028789852@qq.com>
2020-06-17 21:06:13 +0800

    添加测试接口

--- example/HttpReports.Dashboard.WebAPP/Startup.cs
+++ example/HttpReports.Dashboard.WebAPP/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -44,6 +45,99 @@ namespace HttpReports.Dashboard.WebAPP
 
                 });
             });
+
+            MapRoute(app);
         }
+
+        public void MapRoute(IApplicationBuilder app)
+        {
+            app.Map("/Trace", builder =>
+            {
+                builder.Run(async context =>
+                {
+                    System.Threading.Thread.Sleep(new Random().Next(111, 5555));
+
+                    HttpClient client = new HttpClient();
+                    var response = await client.GetStringAsync("http://localhost:" + context.Connection.LocalPort + "/Test1");
+                    await context.Response.WriteAsync(response);
+
+                });
+
+            });
+
+            app.Map("/Test1", builder =>
+            { 
+                builder.Run(async context =>
+                { 
+                    System.Threading.Thread.Sleep(new Random().Next(111,5555));
+
+                    HttpClient client = new HttpClient();
+                    var response = await client.GetStringAsync("http://localhost:"+context.Connection.LocalPort+"/Test2");
+                    await context.Response.WriteAsync(response);
+
+                });
+
+            });
+
+            app.Map("/Test2", builder =>
+            {
+                builder.Run(async context =>
+                {
+                    System.Threading.Thread.Sleep(new Random().Next(111, 5555));
+
+                    HttpClient client = new HttpClient();
+                    var response = await client.GetStringAsync("http://localhost:" + context.Connection.LocalPort + "/Test3");
+                    await context.Response.WriteAsync(response);
+
+                });
+
+            });
+
+            app.Map("/Test3", builder =>
+            {
+                builder.Run(async context =>
+                {
+                    System.Threading.Thread.Sleep(new Random().Next(111, 5555));
+
+                    HttpClient client = new HttpClient();
+                    var response = await client.GetStringAsync("http://localhost:" + context.Connection.LocalPort + "/Test4");
+                    await context.Response.WriteAsync(response);
+
+                });
+
+            });
+
+
+            app.Map("/Test4", builder =>
+            {
+                builder.Run(async context =>
+                {
+                    System.Threading.Thread.Sleep(new Random().Next(111, 5555));
+
+                    HttpClient client = new HttpClient();
+                    var response = await client.GetStringAsync("http://localhost:" + context.Connection.LocalPort + "/Test5");
+                    await context.Response.WriteAsync(response);
+
+                });
+
+            });
+
+
+            app.Map("/Test5", builder => {
+
+                builder.Run(async context => {
+
+                    System.Threading.Thread.Sleep(new Random().Next(111, 5555));
+
+                    await context.Response.WriteAsync("ok");
+
+                    return;
+
+                });
+
+            });
+
+        }
+
     }
 }
