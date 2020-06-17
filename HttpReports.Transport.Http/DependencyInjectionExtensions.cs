6ed2c65f254294fcddfe4784a3ee2a426b0e4855
Li Shuai <lishuai@diyibox.com>
2020-06-17 19:02:44 +0800

    HttpTransPort

---
+++ HttpReports.Transport.Http/DependencyInjectionExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpReports.Transport.Http
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IHttpReportsBuilder UseGrpcReportsTransport(this IHttpReportsBuilder builder, IConfiguration configuration)
+        {
+            builder.Services.AddOptions().Configure<GrpcReportsTransportOptions>(configuration);
+            return builder.UseGrpcReportsTransport();
+        }
+
+        public static IHttpReportsBuilder UseGrpcReportsTransport(this IHttpReportsBuilder builder, Action<GrpcReportsTransportOptions> options)
+        {
+            builder.Services.AddOptions().Configure(options);
+            return builder.UseGrpcReportsTransport();
+        }
+
+        public static IHttpReportsBuilder UseGrpcReportsTransport(this IHttpReportsBuilder builder)
+        {
+            builder.Services.RemoveAll<IReportsTransport>();
+            builder.Services.AddSingleton<IReportsTransport, GrpcReportsTransport>();
+            builder.Services.RemoveAll<IModelCreator>();
+            builder.Services.AddSingleton<IModelCreator, ModelCreator>();
+            return builder;
+        }
+    }
+}
---
+++ HttpReports.Transport.Http/HttpTransportOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpReports.Transport.Http
+{
+    public class HttpTransportOptions
+    {
+        public Uri CollectorAddress { get; set; }
+
+        public HttpTransportOptions Value => this;
+
+    }
+}
