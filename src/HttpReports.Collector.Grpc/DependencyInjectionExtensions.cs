86d5fdb21440b5738a4c3e61d5ad86645b27dca1
Li Shuai <lishuai@diyibox.com>
2020-11-24 16:47:46 +0800

    grpc transport complete

---
+++ src/HttpReports.Collector.Grpc/DependencyInjectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HttpReports.Collector.Grpc
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IHttpReportsBuilder AddGrpcCollector(this IHttpReportsBuilder builder)
+        {
+            builder.Services.AddGrpc();
+            return builder;
+        }
+
+        public static GrpcServiceEndpointConventionBuilder MapGrpcCollector(this IEndpointRouteBuilder builder)
+        {
+            return builder.MapGrpcService<GrpcCollectorService>();
+        }
+    }
+}
---
+++ src/HttpReports.Collector.Grpc/GrpcCollectorService.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading.Tasks; 
+using Grpc.Core;
+using HttpReports.Core;
+using HttpReports.Storage.Abstractions;
+
+namespace HttpReports.Collector.Grpc
+{
+    public class GrpcCollectorService : GrpcCollector.GrpcCollectorBase
+    {
+        public readonly IHttpReportsStorage _storage;
+
+        public GrpcCollectorService(IHttpReportsStorage storage)
+        {
+            _storage = storage;
+        } 
+
+        public override async Task<WriteReply> WritePerformance(Performance performance, ServerCallContext context)
+        {
+            Core.Models.Performance model = new Core.Models.Performance() {
+            
+                Id = performance.Id,
+                CreateTime = new System.DateTime(performance.CreateTimeStamp),
+                GCGen0 = performance.GCGen0,
+                GCGen1 = performance.GCGen1,
+                GCGen2 = performance.GCGen2,
+                HeapMemory = performance.HeapMemory,
+                Service = performance.Service,
+                Instance = performance.Instance,
+                PendingThreadCount = performance.PendingThreadCount,
+                ProcessCPU = performance.ProcessCPU,
+                ProcessMemory = performance.ProcessMemory,
+                ThreadCount = performance.ThreadCount 
+            }; 
+
+            await _storage.AddPerformanceAsync(model);
+
+            return new WriteReply() { Code = 0 }; 
+        }
+
+
+        public override async Task<WriteReply> WriteRequest(RequestInfoPack request, ServerCallContext context)
+        {
+            foreach (var item in request.Data)
+            {
+                var info = new Core.Models.RequestInfo {
+                
+                     Id = item.RequestInfo.Id,
+                     Service = item.RequestInfo.Service,
+                     Instance = item.RequestInfo.Instance,
+                     CreateTime = new System.DateTime(item.RequestInfo.CreateTimeStamp),
+                     LoginUser = item.RequestInfo.LoginUser,
+                     Method = item.RequestInfo.Method,
+                     Milliseconds = item.RequestInfo.Milliseconds,
+                     ParentId = item.RequestInfo.ParentId,
+                     ParentService = item.RequestInfo.ParentService,
+                     RemoteIP = item.RequestInfo.RemoteIP,
+                     RequestType = item.RequestInfo.RequestType,
+                     Route = item.RequestInfo.Route,
+                     StatusCode = item.RequestInfo.StatusCode,
+                     Url = item.RequestInfo.Url 
+                };
+
+                var detail = new Core.Models.RequestDetail {
+
+                    Id = item.RequestDetail.Id,
+                    RequestId = item.RequestDetail.RequestId,
+                    Cookie = item.RequestDetail.Cookie,
+                    CreateTime = new System.DateTime(item.RequestDetail.CreateTimeStamp),
+                    ErrorMessage = item.RequestDetail.ErrorMessage,
+                    ErrorStack = item.RequestDetail.ErrorStack,
+                    Header = item.RequestDetail.Header,
+                    QueryString = item.RequestDetail.QueryString,
+                    RequestBody = item.RequestDetail.RequestBody,
+                    ResponseBody = item.RequestDetail.ResponseBody,
+                    Scheme = item.RequestDetail.Scheme 
+                }; 
+
+                RequestBag bag = new RequestBag(info,detail);
+
+                await _storage.AddRequestInfoAsync(bag);
+            }
+
+            return new WriteReply() { Code = 0 };
+        }
+    }
+}
