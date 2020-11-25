15b866a3c7cf395b50f65b1342cdc81c336a5916
Li Shuai <lishuai@diyibox.com>
2020-11-25 19:49:31 +0800

    publish 2.5.0

--- build/BuildScript.cs
+++ build/BuildScript.cs
@@ -90,8 +90,7 @@ namespace BuildScript
             context.CreateTarget("Default")
              .SetDescription("Runs all targets.")
              .SetAsDefault()
-             .DependsOn(clean, restore, build, pack,push,push2); 
-
+             .DependsOn(clean, restore, build, pack,push, push2);  
         }
 
         private void NugetPush(ITaskContext context)
--- example/HttpReports.Demo/Startup.cs
+++ example/HttpReports.Demo/Startup.cs
@@ -3,13 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting; 
+using Microsoft.OpenApi.Models; 
 
 namespace HttpReports.Demo
 {
@@ -25,8 +20,8 @@ namespace HttpReports.Demo
       
         public void ConfigureServices(IServiceCollection services)
         { 
-            //services.AddHttpReports().AddGrpcTransport();
-            services.AddHttpReportsDashboard().AddGrpcCollector().AddMySqlStorage(); 
+            //services.AddHttpReports().AddHttpTransport();
+            services.AddHttpReportsDashboard().AddMySqlStorage(); 
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
