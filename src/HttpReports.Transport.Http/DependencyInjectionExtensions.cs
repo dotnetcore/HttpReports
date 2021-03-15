1b0de6d86cc6b251bd445557bf2b1523fc601605
renqi <ren8179@126.com>
2021-03-15 14:11:13 +0800

    兼容配置项被修改(重命名/嵌套)的情况

--- src/HttpReports.Transport.Http/DependencyInjectionExtensions.cs
+++ src/HttpReports.Transport.Http/DependencyInjectionExtensions.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http.Headers;
-using System.Text;
-using HttpReports;
+﻿using HttpReports;
 using HttpReports.Core;
 using HttpReports.Transport.Http;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection.Extensions; 
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class DependencyInjectionExtensions
-    { 
+    {
         [Obsolete("Use AddHttpTransport instead")]
         public static IHttpReportsBuilder UseHttpTransport(this IHttpReportsBuilder builder)
         {
@@ -19,40 +16,42 @@ namespace Microsoft.Extensions.DependencyInjection
             return builder.AddHttpTransportService();
         }
 
-
         [Obsolete("Use AddHttpTransport instead")]
         public static IHttpReportsBuilder UseHttpTransport(this IHttpReportsBuilder builder, Action<HttpTransportOptions> options)
         {
             builder.Services.AddOptions().Configure(options);
             return builder.AddHttpTransportService();
-        } 
-
+        }
 
         public static IHttpReportsBuilder AddHttpTransport(this IHttpReportsBuilder builder)
-        { 
+        {
             builder.Services.AddOptions().Configure<HttpTransportOptions>(builder.Configuration.GetSection("Transport"));
             return builder.AddHttpTransportService();
         }
 
+        public static IHttpReportsBuilder AddHttpTransport(this IHttpReportsBuilder builder, IConfiguration configuration)
+        {
+            return builder.AddHttpTransportService();
+        }
+
         public static IHttpReportsBuilder AddHttpTransport(this IHttpReportsBuilder builder, Action<HttpTransportOptions> options)
-        { 
+        {
             builder.Services.AddOptions().Configure(options);
             return builder.AddHttpTransportService();
         }
 
         private static IHttpReportsBuilder AddHttpTransportService(this IHttpReportsBuilder builder)
         {
-            builder.Services.AddHttpClient(BasicConfig.HttpReportsHttpClient,client => {
-
-                client.DefaultRequestHeaders.Clear(); 
+            builder.Services.AddHttpClient(BasicConfig.HttpReportsHttpClient, client =>
+            {
+                client.DefaultRequestHeaders.Clear();
                 client.Timeout = TimeSpan.FromSeconds(5);
 
             });
 
             builder.Services.RemoveAll<IReportsTransport>();
-            builder.Services.AddSingleton<IReportsTransport, HttpTransport>(); 
+            builder.Services.AddSingleton<IReportsTransport, HttpTransport>();
             return builder;
         }
     }
-
 }
